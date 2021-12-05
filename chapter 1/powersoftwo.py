963322f2a8e945c18bd507535d7297519262f152
oktamorph <oktamorph@gmail.com>
2021-12-05 21:01:20 +0100

    update the 'tenhellos.py' and 'powersoftwo.py' files

--- chapter 1/powersoftwo.py
+++ chapter 1/powersoftwo.py
@@ -3,9 +3,8 @@ import stdio
 
 n = int(sys.argv[1])
 power = 1
-i = 0
 
-while i <= n:
+for i in range(n + 1):
     stdio.writeln(str(i) + ' ' + str(power))
-    power = 2 * power
-    i = i + 1
+    power *= 2
+
