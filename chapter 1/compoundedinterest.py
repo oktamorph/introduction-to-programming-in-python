f197efbe6185eeb42c54c1efc84826696df423de
oktamorph <oktamorph@gmail.com>
2022-01-05 22:00:20 +0100

    update the 'compoundedinterest.py' file

--- chapter 1/compoundedinterest.py
+++ chapter 1/compoundedinterest.py
@@ -6,7 +6,8 @@ e = math.e
 P = int(sys.argv[1]) # principal
 r = int(sys.argv[2]) # rate
 t = int(sys.argv[3]) # year
+months = 12
 
 result = P * e ** (r * t)
-stdio.writeln(result);
-
+stdio.writeln(result)
+stdio.writeln("Monthly payment: " + str(result / months))
