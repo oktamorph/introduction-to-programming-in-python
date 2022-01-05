6d4eaf7bdd2ced635d0491c1f8e3897838768e2b
oktamorph <oktamorph@gmail.com>
2022-01-05 22:07:20 +0100

    update the 'divisionpattern.py' file

--- chapter 1/divisorpattern.py
+++ chapter 1/divisorpattern.py
@@ -12,3 +12,19 @@ for i in range(1, n + 1):
         else:
             stdio.write(' ')
     stdio.writeln(i)
+stdio.writeln()
+
+i = 1
+j = 1
+while i < n + 1:
+    # Write the ith line.
+    while j < n + 1:
+        # Write the jth entry in the ith line.
+        if (i % j == 0) or (j % i == 0):
+            stdio.write('* ')
+        else:
+            stdio.write(' ')
+        j += 1
+    stdio.writeln(i)
+    j = 1
+    i += 1
